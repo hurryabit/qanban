9907df307663c5ad7878ad4c19e0af24928b433c
Martin Huschenbett <martin.huschenbett@posteo.me>
2020-03-19 23:33:07 +0100

    Use qured-client in qanban-node (#18)

--- qured-client/src/index.ts
+++ qured-client/src/index.ts
@@ -2,46 +2,52 @@ import * as jtv from '@mojotech/json-type-validation';
 import Emittery from 'emittery';
 import WebSocket from 'ws';
 
-export type Config<Payload> = {
-  router: string;
-  login: string;
-  payloadDecoder?: jtv.Decoder<Payload>;
-}
+export type Config<Payload, Party extends string = string> = {
+  readonly router: string;
+  readonly login: Party;
+  readonly partyDecoder?: jtv.Decoder<Party>;
+  readonly payloadDecoder?: jtv.Decoder<Payload>;
+} & (string extends Party ? {} : { readonly partyDecoder: jtv.Decoder<Party> })
+// NOTE(MH): The purpose of the intersection with the conditional type is to
+// make `partyDecoder` non-optional when `Party` is _not_ `string`.
 
-export type Message<Payload> = {
-  sender: string;
-  receivers: string[];
+export type Message<Payload, Party extends string = string> = {
+  sender: Party;
+  receivers: Party[];
   payload: Payload;
 }
 
-export type Events<Payload> = {
-  message: Message<Payload>;
+export type Events<Payload, Party extends string = string> = {
+  message: Message<Payload, Party>;
   error: unknown;
   close: number;
 }
 
 export type EmptyEvents = "open"
 
-export default class QuredClient<Payload> extends Emittery.Typed<Events<Payload>, EmptyEvents> {
+export default class QuredClient<Payload, Party extends string = string> extends Emittery.Typed<Events<Payload, Party>, EmptyEvents> {
   private socket: WebSocket;
-  private messageDecoder: jtv.Decoder<Message<Payload>>;
+  private login: Party;
+  private messageDecoder: jtv.Decoder<Message<Payload, Party>>;
 
-  constructor(config: Config<Payload>) {
+  constructor(config: Config<Payload, Party>) {
     super();
     const socket = new WebSocket(config.router);
     this.socket = socket;
+    this.login = config.login;
+    const partyDecoder = config.partyDecoder ?? jtv.string().map(party => party as Party);
     this.messageDecoder = jtv.object({
-      sender: jtv.string(),
-      receivers: jtv.array(jtv.string()),
+      sender: partyDecoder,
+      receivers: jtv.array(partyDecoder),
       payload: config.payloadDecoder ?? jtv.anyJson(),
     });
     socket.on("open", () => {
       socket.on("ping", () => socket.pong());
       socket.on("message", rawMessage => {
         try {
-          console.log(`incoming message: ${rawMessage}`);
-          const message: Message<Payload> =
-            this.messageDecoder.runWithException(JSON.parse(rawMessage.toString()));
+          const json = JSON.parse(rawMessage.toString());
+          console.log('incoming message:', json);
+          const message: Message<Payload, Party> = this.messageDecoder.runWithException(json);
           // eslint-disable-next-line @typescript-eslint/no-floating-promises
           this.emit("message", message);
         } catch (error) {
@@ -63,10 +69,13 @@ export default class QuredClient<Payload> extends Emittery.Typed<Events<Payload>
     });
   }
 
-  send(message: Message<Payload>): void {
-    const rawMessage = JSON.stringify(message);
-    console.log(`outgoing message: ${rawMessage}`);
-    this.socket.send(rawMessage);
+  send(message: Message<Payload, Party>): void {
+    if (message.sender !== this.login) {
+      throw Error(`sender '${message.sender}' of message does not match login '${this.login}'`);
+    }
+    const json = this.messageDecoder.runWithException(message);
+    console.log('outgoing message:', json);
+    this.socket.send(JSON.stringify(json));
   }
 
   close(): void {
